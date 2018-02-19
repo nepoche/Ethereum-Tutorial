72830b233a172cf1e3d75b548954d2dab574b5a8
Nathan Barnavon <nepoche@yahoo.com>
2018-02-19 14:59:37 -0500

    add restrictions and cancellation

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -14,23 +14,49 @@ contract Escrow {
     }
 
     function postItem() payable {
+        require(state == State.Inactive);
+        require(msg.value % 2 == 0);
         seller = msg.sender;
         price = msg.value / 2;
         state = State.Created;
     }
 
+    function abort() {
+        require(msg.sender == seller);
+        require(state == State.Created);
+        seller.send(this.balance);
+		state = State.Inactive;
+		price = 0;
+		seller = 0;
+    }
+
     function purchase() payable {
+        require(state == State.Created);
+        require(msg.value == 2 * price);
         buyer = msg.sender;
         state = State.Locked;
     }
 
     function confirmed() {
+        require(state == State.Locked);
+        require(msg.sender == buyer);
         buyer.send(price);
         seller.send(this.balance);
         state = State.Inactive;
         price = 0;
         seller = 0;
         buyer = 0;
+    }
+
+    function refundBuyer() {
+        require(state == State.Locked);
+        require(msg.sender == seller);
+        buyer.send(2 * price);
+		seller.send(this.balance);
+		state = State.Inactive;
+		price = 0;
+		seller = 0;
+		buyer = 0;
     }
 
 }
