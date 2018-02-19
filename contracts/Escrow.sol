817e4b28c3d1dd6a5adc4c69117dd7e7b6ef46e4
Nathan Barnavon <nepoche@yahoo.com>
2018-02-19 15:11:08 -0500

    Added modifiers and events

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -9,54 +9,71 @@ contract Escrow {
 	enum State { Created, Locked, Inactive }
 	State public state;
 
+    event Created(address seller, uint price);
+	event Aborted();
+	event Purchased(address buyer);
+	event Confirmed();
+	event Refunded();
+
+    modifier onlyBuyer() {
+		require(msg.sender == buyer);
+		_;
+	}
+
+	modifier onlySeller() {
+		require(msg.sender == seller);
+		_;
+	}
+
+	modifier inState(State s) {
+		require(state == s);
+		_;
+	}
+
     function Escrow() {
         state = State.Inactive;
     }
 
-    function postItem() payable {
-        require(state == State.Inactive);
+    function postItem() payable inState(State.Inactive) {
         require(msg.value % 2 == 0);
         seller = msg.sender;
         price = msg.value / 2;
         state = State.Created;
+        Created(seller, price);
     }
 
-    function abort() {
-        require(msg.sender == seller);
-        require(state == State.Created);
+    function abort() inState(State.Created) onlySeller {
         seller.send(this.balance);
 		state = State.Inactive;
 		price = 0;
 		seller = 0;
+        Aborted();
     }
 
-    function purchase() payable {
-        require(state == State.Created);
+    function purchase() payable inState(State.Created) {
         require(msg.value == 2 * price);
         buyer = msg.sender;
         state = State.Locked;
+        Purchased(buyer);
     }
 
-    function confirmed() {
-        require(state == State.Locked);
-        require(msg.sender == buyer);
+    function confirmed() inState(State.Locked) onlyBuyer {
         buyer.send(price);
         seller.send(this.balance);
         state = State.Inactive;
         price = 0;
         seller = 0;
         buyer = 0;
+        Confirmed();
     }
 
-    function refundBuyer() {
-        require(state == State.Locked);
-        require(msg.sender == seller);
+    function refundBuyer() inState(State.Locked) onlySeller {
         buyer.send(2 * price);
 		seller.send(this.balance);
 		state = State.Inactive;
 		price = 0;
 		seller = 0;
 		buyer = 0;
+        Refunded();
     }
-
 }
